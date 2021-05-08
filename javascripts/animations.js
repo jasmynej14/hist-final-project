e8098e3093a5810b9c2c37be24ebd0e9e5280d14
jasmynej14 <jasmynej14@brandeis.edu>
2021-05-08 15:24:07 -0400

    amimations

---
+++ javascripts/animations.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".info-body").hide()
+    $(".notable-body").hide()
+    $(".info-header").click(function(){
+        if($(".info-body").is(":visible")){
+            $(".info-body").slideUp("slow")
+        }
+        else{
+            $(".info-body").slideDown("slow")
+        }
+        
+    })
+
+    $(".notable-header").click(function(){
+        if($(".notable-body").is(":visible")){
+            $(".notable-body").slideUp("slow")
+        }
+        else{
+            $(".notable-body").slideDown("slow")
+        }
+    })
+})
