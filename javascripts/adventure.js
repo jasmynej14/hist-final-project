dc20f5f6b032a651cdc8049fddeeb1d948c8b531
jasmynej14 <jasmynej14@brandeis.edu>
2021-05-10 23:48:50 -0400

    interactions for game

--- javascripts/adventure.js
+++ javascripts/adventure.js
@@ -1,39 +1,54 @@
 $(document).ready(function(){
  $(".welcome").hide()
-   $(".theme-night").hide()
-   $(".order-food").hide()
-   $(".music").hide()
-   $(".ending").hide()
-  welcome_box = $(".welcome")
+ $(".theme-night").hide()
+ $(".order-food").hide()
+ $(".music").hide()
+ $(".ending").hide()
+ welcome_box = $(".welcome")
   theme_night = $(".theme-night")
   order_food = $(".order-food")
-  music  = $(".music")
-  ending = $(".ending")
+   music  = $(".music")
+   ending = $(".ending")
 
   var today = new Date()
 
   $("#start-button").click(function(){
     $(".get-info").hide()
    //alert("kisss my ass")
-   $(".get-info").hide()
-   //$(".welcome").show()
+   $(".welcome-header").append($("#name").val())
+   $(".welcome").show()
    //$(".welcome_box").html(welcome_box)
-   //$(".welcome-header").append($("#name").val())
+   
   });
 
  
-  $("#enter-button").click(function(){
-    alert("clicked")
+   $("#enter-button").click(function(){
+     welcome_box.hide()
     theme_night.show()
     var day = today.getDay() 
-    var nights = ["Regular Dance Night","Ladies' Night","Regular Dance Night (400 Club)","Society Night","Kitchen Mechanic's Nights","Society Night","Square Night"]
-    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-    $(".current-night").append(nights[day])
-    $(".game").html(theme_night)
-    
-
-    
+     var nights = ["Regular Dance Night","Ladies' Night","Regular Dance Night (400 Club)","Society Night","Kitchen Mechanic's Nights","Society Night","Square Night"]
+     var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+     $(".current-night").append(nights[day]+" ("+days[day]+")")
+    //$(".game").html(theme_night)
   });
 
+  $("#start-food").click(function(){ //show order food
+    theme_night.hide()
+    order_food.show()
+  })
 
+  $("#order-food").click(function(){ //show music
+    order_food.hide()
+    music.show()
+  })
+ 
+  $("#end-button").click(function(){
+    music.hide()
+    ending.show()
+  })
+  
+  $("#start-over").click(function(){
+    ending.hide()
+    $(".get-info").show()
+  })
 })
